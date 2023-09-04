5954a8d807454bfc035a91d65fbe227e78397095
Justin <34551787+HJK-X@users.noreply.github.com>
2023-09-03 22:42:04 -0700

    ok well im commiting this to remember it for later

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Home from './components/Home';
 function App() {
   return (
     <div className="App">
@@ -19,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React  
+          Learn React
         </a>
       </header>
     </div>
