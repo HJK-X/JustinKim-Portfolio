ac92043595e5d74a20fb884dfc1c9a02277fdc7d
Justin <34551787+HJK-X@users.noreply.github.com>
2023-09-25 20:14:09 -0700

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,18 +8,7 @@ function App() {
       <header className="App-header">
           <Home />
           {/* Add other components and routes here */}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          
       </header>
     </div>
   );
