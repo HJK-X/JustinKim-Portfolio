d9e8f3b526074015dcbbf65862352403a4e2608a
Justin <34551787+HJK-X@users.noreply.github.com>
2023-09-27 20:02:29 -0700

    working on it

---
+++ portfolio/src/components/TopRepo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function TopRepo({ name, description, url }) {
+  return (
+    <div className="top-repo">
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        View on GitHub
+      </a>
+    </div>
+  );
+}
+
+export default TopRepo;
